c110878abb06e9a23189d63b14d903572f3b2312
xingcheng <xingcheng@lyzdfintech.com>
2021-12-15 18:07:23 +0800

    添加权限配置类

---
+++ bootdemo/src/main/java/com/xc/bootdemo/config/MySecurityConfig.java
@@ -0,0 +1,14 @@
+package com.xc.bootdemo.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers().hasAnyAuthority();
+    }
+}
