dfb4ca8cf59304dd9620600fdf51e1fcfd20aed8
xingcheng <xingcheng@lyzdfintech.com>
2021-12-16 16:03:08 +0800

    获取数据源对象

--- bootdemo/src/main/java/com/xc/bootdemo/config/MyWebMvcConfigurer.java
+++ bootdemo/src/main/java/com/xc/bootdemo/config/MyWebMvcConfigurer.java
@@ -1,17 +1,25 @@
 package com.xc.bootdemo.config;
 
 import com.xc.bootdemo.interceptor.MyAuthInterceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
+import javax.sql.DataSource;
+
+@Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
     @Autowired
     private MyAuthInterceptor myAuthInterceptor;
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -22,4 +30,11 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
 
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        DataSource ds = sqlSessionFactory.getConfiguration().getEnvironment().getDataSource();
+        return new JdbcUserDetailsManager(ds);
+    }
+
 }
